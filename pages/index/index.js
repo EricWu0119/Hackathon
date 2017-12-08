9aba0dff4b8051032502d6ddd9caa5a7864b6dfe
ZHI GUO DU <duzhiguo@cn.ibm.com>
2017-12-08 16:00:09 +0800

    wxRequest demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,14 +14,13 @@ Page({
     })
   },
 
-  onLoad: function () {
+  onLoad: function (options) {
     // console.log('themes.onLoad');
     var that = this;
-    // 加载数据
-    wx.request({
-      // url: 'http://news-at.zhihu.com/api/4/themes',
-      url: 'https://www.todaynowork.group/wechat-du-1.0/courseSchedule/12',
-      header: { "Content-Type": "application/json" },
+    var courseId =12;
+    if (options.id)
+      courseId = options.id;
+    let requestConf = {
       success: function (res) {
         var data = res.data;
         console.log(data);
@@ -32,6 +31,25 @@ Page({
       fail: function (error) {
         console.log(error)
       }
-    });
+    };
+
+    app.wxRequest("/courseSchedule/" + courseId, requestConf);
+    // 加载数据
+
+    // wx.request({
+    //   // url: 'http://news-at.zhihu.com/api/4/themes',
+    //   url: 'https://www.todaynowork.group/wechat-du-1.0/courseSchedule/12',
+    //   header: { "Content-Type": "application/json" },
+    //   success: function (res) {
+    //     var data = res.data;
+    //     console.log(data);
+    //     var temp = [];
+    //     temp.push(data);
+    //     that.setData({ list: temp });
+    //   },
+    //   fail: function (error) {
+    //     console.log(error)
+    //   }
+    // });
   }
 })
